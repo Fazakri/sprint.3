1c7e5107c55d7a26270b9f3301fe09e1ef55bbee
Fazakri <86667861+Fazakri@users.noreply.github.com>
2021-08-03 08:49:05 +0700

    Add files via upload

---
+++ topik1/tugas-5.js/default/export.js
@@ -0,0 +1,9 @@
+const antrian = (awal, tengah, terakhir) => {
+    let antrian = {
+        barisDepan : depan,
+        barisTengah : tengah,
+        barisTerakhir :terakhir,
+    }
+    return antrian
+}
+export default antrian
